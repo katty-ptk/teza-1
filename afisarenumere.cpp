98831a84d0e32cf1b325709b8747092c67db8ad3
Katty Lazar <katy.lazar05@gmail.com>
2021-12-07 16:43:20 +0200

    solved first problem

--- afisarenumere.cpp
+++ afisarenumere.cpp
@@ -1,8 +1,30 @@
 /*
 
     Se dă un număr natural n. Afișați în ordine crescătoare primele n numere naturale nenule.
+
     ex: n = 5
     output: 1 2 3 4 5
 
  */
 
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // n - cate numere sunt
+    // x - iau nr
+    // i - for
+    int n, i, x; 
+
+    cout << "n = "; cin >> n;   // citeste n ( cate nr sunt )
+
+    // Se dă un număr natural n. Afișați în ordine crescătoare primele n numere naturale nenule.
+    for ( i = 1; i <= n; i++ ) {    // i de la 1 la n
+        cout << i << " ";   // se afiseaza i
+    }
+
+    cout << endl;  
+
+    return 0;
+}
