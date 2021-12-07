05e48db21c2a972d9dfd3818570c5b892eb1e81b
Katty Lazar <katy.lazar05@gmail.com>
2021-12-07 16:57:16 +0200

    solved 2nd problem

--- afisarenumere2.cpp
+++ afisarenumere2.cpp
@@ -6,4 +6,30 @@
     output:
         1 2 3 4 5
         5 4 3 2 1
+
 */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // n - cate nr sunt
+    // i, j - contori
+    int n, i, j;
+    cout << "n = "; cin >> n; // citire n
+
+    for ( i = 1; i <= n; i++ ) {    // i de la 1 la n
+        cout << i << " ";   // afisam i
+    }
+    
+    cout << endl;   // trecere pe rand nou
+
+    for ( j = n; j >= 1; j-- ) {    // j de la n la 1
+        cout << j << " ";   // afisam j
+    }
+
+    cout << endl;
+
+    return 0;
+}
